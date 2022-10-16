6979f4b7b13e2df305b81132e2e44993f39eb489
ycc <1378983614@qq.com>
2022-10-16 22:52:21 +0800

    ch05: lua operator

---
+++ go/ch05/src/luago/state/api_misc.go
@@ -0,0 +1,28 @@
+package state
+
+func (state *luaState) Len(idx int) {
+	val := state.stack.get(idx)
+	if s, ok := val.(string); ok {
+		state.stack.push(int64(len(s)))
+	} else {
+		panic("length error")
+	}
+}
+
+func (state *luaState) Concat(n int) {
+	if n == 0 {
+		state.stack.push("")
+	} else if n >= 2 {
+		for i := 1; i < n; i++ {
+			if state.IsString(-1) && state.IsString(-2) {
+				s2 := state.ToString(-1)
+				s1 := state.ToString(-2)
+				state.stack.pop()
+				state.stack.pop()
+				state.stack.push(s1 + s2)
+				continue
+			}
+			panic("concatenation error")
+		}
+	}
+}
---
+++ go/ch05/src/luago/state/api_stack.go
@@ -0,0 +1,76 @@
+package state
+
+func (state *luaState) GetTop() int {
+	return state.stack.top
+}
+
+func (state *luaState) AbsIndex(idx int) int {
+	return state.stack.absIndex(idx)
+}
+
+func (state *luaState) CheckStack(n int) bool {
+	state.stack.check(n)
+	return true
+}
+
+func (state *luaState) Pop(n int) {
+	for i := 0; i < n; i++ {
+		state.stack.pop()
+	}
+}
+
+func (state *luaState) Copy(fromIdx, ToIdx int) {
+	val := state.stack.get(fromIdx)
+	state.stack.set(ToIdx, val)
+}
+
+func (state *luaState) PushValue(idx int) {
+	val := state.stack.get(idx)
+	state.stack.push(val)
+}
+
+func (state *luaState) Replace(idx int) {
+	val := state.stack.pop()
+	state.stack.set(idx, val)
+}
+
+func (state *luaState) Insert(idx int) {
+	state.Rotate(idx, 1)
+}
+
+func (state *luaState) Remove(idx int) {
+	state.Rotate(idx, -1)
+	state.Pop(1)
+}
+
+func (state *luaState) Rotate(idx, n int) {
+	t := state.stack.top - 1
+	p := state.stack.absIndex(idx) - 1
+	var m int
+	if n >= 0 { //n的符号代表旋转的方向，>0向栈顶 < 0向栈底
+		m = t - n
+	} else {
+		m = p - n - 1
+	}
+	state.stack.Reverse(p, m)
+	state.stack.Reverse(m+1, t)
+	state.stack.Reverse(p, t)
+}
+
+func (state *luaState) SetTop(idx int) {
+	newTop := state.stack.absIndex(idx)
+	if newTop < 0 {
+		panic("stack underflow")
+	}
+
+	n := state.stack.top - newTop
+	if n > 0 {
+		for i := 0; i < n; i++ {
+			state.stack.pop()
+		}
+	} else if n < 0 {
+		for i := 0; i > n; i-- {
+			state.stack.push(nil)
+		}
+	}
+}
