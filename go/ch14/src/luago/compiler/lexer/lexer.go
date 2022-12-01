fc1c6caef4c33e5ac6067d4ab9e0f0d6b30d5072
ycc <1378983614@qq.com>
2022-12-01 19:24:07 +0800

    ch14: lexical analysis

---
+++ go/ch14/src/luago/compiler/lexer/lexer.go
@@ -0,0 +1,433 @@
+package lexer
+
+import (
+	"bytes"
+	"fmt"
+	"regexp"
+	"strconv"
+	"strings"
+)
+
+var reOpeningLongBracket = regexp.MustCompile(`^\[=*\]`)
+var reNewLine = regexp.MustCompile("\r\n|\n\r|\n|\r")
+var reShortStr = regexp.MustCompile(`(?s)(^'(\\\\|\\'|\\\n|\\z\s*|[^'\n])*')|(^"(\\\\|\\"|\\\n|\\z\s*|[^"\n])*")`)
+var reDecEscapeSeq = regexp.MustCompile(`^\\[0-9]{1,3}`)
+var reHexEscapeSeq = regexp.MustCompile(`^\\[0-9a-fA-F]{2}`)
+var reUnicodeEscapeSeq = regexp.MustCompile(`^\\u\{[0-9a-fA-F]+\}`)
+var reIdentifier = regexp.MustCompile(`^[_\d\w]+`)
+var reNumber = regexp.MustCompile(`^0[xX][0-9a-fA-F]*(\.[0-9a-fA-F]*)?([pP][+\-]?[0-9]+)?|^[0-9]*(\.[0-9]*)?([eE][+\-]?[0-9]+)?`)
+
+type Lexer struct {
+	chunk         string // 源代码
+	chunkName     string // 源文件名
+	line          int    // 当前行号
+	nextToken     string
+	nextTokenKind int
+	nextTokenLine int
+}
+
+func NewLexer(chunk, chunkName string) *Lexer {
+	return &Lexer{chunk, chunkName, 1, "", 0, 0}
+}
+
+func (lexer *Lexer) LookAhead() int {
+	if lexer.nextTokenLine > 0 { // 判断缓存里是是否有下一个token的信息 有的话直接返回
+		return lexer.nextTokenKind
+	}
+	currentLine := lexer.line
+	line, kind, token := lexer.NextToken()
+	lexer.line = currentLine
+	lexer.nextTokenLine = line
+	lexer.nextTokenKind = kind
+	lexer.nextToken = token
+	return kind
+}
+
+func (lexer *Lexer) NextToken() (line, kind int, token string) {
+	if lexer.nextTokenLine > 0 {
+		line = lexer.nextTokenLine
+		kind = lexer.nextTokenKind
+		token = lexer.nextToken
+		lexer.line = lexer.nextTokenLine
+		lexer.nextTokenLine = 0
+		return
+	}
+	lexer.skipWhiteSpaces()
+	if len(lexer.chunk) == 0 {
+		return lexer.line, TOKEN_EOF, "EOF"
+	}
+
+	switch lexer.chunk[0] {
+	case ';':
+		lexer.next(1)
+		return lexer.line, TOKEN_SEP_SEMI, ";"
+	case ',':
+		lexer.next(1)
+		return lexer.line, TOKEN_SEP_COMMA, ","
+	case '(':
+		lexer.next(1)
+		return lexer.line, TOKEN_SEP_LPAREN, "("
+	case ')':
+		lexer.next(1)
+		return lexer.line, TOKEN_SEP_RPAREN, ")"
+	case ']':
+		lexer.next(1)
+		return lexer.line, TOKEN_SEP_RBRACK, "]"
+	case '{':
+		lexer.next(1)
+		return lexer.line, TOKEN_SEP_LCURLY, "{"
+	case '}':
+		lexer.next(1)
+		return lexer.line, TOKEN_SEP_RCURLY, "}"
+	case '+':
+		lexer.next(1)
+		return lexer.line, TOKEN_OP_ADD, "+"
+	case '-':
+		lexer.next(1)
+		return lexer.line, TOKEN_OP_MINUS, "-"
+	case '*':
+		lexer.next(1)
+		return lexer.line, TOKEN_OP_MUL, "*"
+	case '^':
+		lexer.next(1)
+		return lexer.line, TOKEN_OP_POW, "^"
+	case '%':
+		lexer.next(1)
+		return lexer.line, TOKEN_OP_MOD, "%"
+	case '&':
+		lexer.next(1)
+		return lexer.line, TOKEN_OP_BAND, "&"
+	case '|':
+		lexer.next(1)
+		return lexer.line, TOKEN_OP_BOR, "|"
+	case '#':
+		lexer.next(1)
+		return lexer.line, TOKEN_OP_LEN, "#"
+	case ':':
+		if lexer.test("::") {
+			lexer.next(2)
+			return lexer.line, TOKEN_SEP_LABEL, "::"
+		} else {
+			lexer.next(1)
+			return lexer.line, TOKEN_SEP_COLON, ":"
+		}
+	case '/':
+		if lexer.test("//") {
+			lexer.next(2)
+			return lexer.line, TOKEN_OP_IDIV, "//"
+		} else {
+			lexer.next(1)
+			return lexer.line, TOKEN_OP_DIV, "/"
+		}
+	case '~':
+		if lexer.test("~=") {
+			lexer.next(2)
+			return lexer.line, TOKEN_OP_NE, "~="
+		} else {
+			lexer.next(1)
+			return lexer.line, TOKEN_OP_WAVE, "~"
+		}
+	case '=':
+		if lexer.test("==") {
+			lexer.next(2)
+			return lexer.line, TOKEN_OP_EQ, "=="
+		} else {
+			lexer.next(1)
+			return lexer.line, TOKEN_OP_ASSIGN, "="
+		}
+	case '<':
+		if lexer.test("<<") {
+			lexer.next(2)
+			return lexer.line, TOKEN_OP_SHL, "<<"
+		} else if lexer.test("<=") {
+			lexer.next(2)
+			return lexer.line, TOKEN_OP_LE, "<="
+		} else {
+			lexer.next(1)
+			return lexer.line, TOKEN_OP_LT, "<"
+		}
+	case '>':
+		if lexer.test(">>") {
+			lexer.next(2)
+			return lexer.line, TOKEN_OP_SHR, ">>"
+		} else if lexer.test(">=") {
+			lexer.next(2)
+			return lexer.line, TOKEN_OP_GE, ">="
+		} else {
+			lexer.next(1)
+			return lexer.line, TOKEN_OP_GT, ">"
+		}
+	case '.':
+		if lexer.test("...") {
+			lexer.next(3)
+			return lexer.line, TOKEN_VARARG, "..."
+		} else if lexer.test("..") {
+			lexer.next(2)
+			return lexer.line, TOKEN_OP_CONCAT, ".."
+		} else if len(lexer.chunk) == 1 || !isDigit(lexer.chunk[1]) {
+			lexer.next(1)
+			return lexer.line, TOKEN_SEP_DOT, "."
+		}
+	case '[':
+		if lexer.test("[[") || lexer.test("[=") {
+			return lexer.line, TOKEN_STRING, lexer.scanLongString()
+		} else {
+			lexer.next(1)
+			return lexer.line, TOKEN_SEP_LBRACK, "["
+		}
+	case '\'', '"':
+		return lexer.line, TOKEN_STRING, lexer.scanShortString()
+	}
+
+	c := lexer.chunk[0]
+	if c == '.' || isDigit(c) {
+		token := lexer.scanNumber()
+		return lexer.line, TOKEN_NUMBER, token
+	}
+	if c == '_' || isLetter(c) {
+		token := lexer.scanIdentifier()
+		if kind, found := keywords[token]; found {
+			return lexer.line, kind, token // keyword
+		} else {
+			return lexer.line, TOKEN_IDENTIFIER, token
+		}
+	}
+
+	lexer.error("unexpected symbol near %q", c)
+	return
+}
+
+func (lexer *Lexer) NextTokenOfKind(kind int) (line int, token string) {
+	line, _kind, token := lexer.NextToken()
+	if kind != _kind {
+		lexer.error("syntax error near '%s", token)
+	}
+	return line, token
+}
+
+func (lexer *Lexer) NextIdentifier() (line int, token string) {
+	return lexer.NextTokenOfKind(TOKEN_IDENTIFIER)
+}
+
+func (lexer *Lexer) Line() int {
+	return lexer.line
+}
+
+func isLetter(c byte) bool {
+	return c >= 'a' && c <= 'z' || c >= 'A' && c <= 'z'
+}
+
+func isDigit(c byte) bool {
+	return c >= '0' && c <= '9'
+}
+
+func (lexer *Lexer) skipWhiteSpaces() {
+	for len(lexer.chunk) > 0 {
+		if lexer.test("--") {
+			lexer.skipComment()
+		} else if lexer.test("\r\n") || lexer.test("\n\r") {
+			lexer.next(2)
+			lexer.line += 1
+		} else if isNewLine(lexer.chunk[0]) {
+			lexer.next(1)
+			lexer.line += 1
+		} else if isWhiteSpace(lexer.chunk[0]) {
+			lexer.next(1)
+		} else {
+			break
+		}
+	}
+}
+
+func (lexer *Lexer) test(s string) bool {
+	return strings.HasPrefix(lexer.chunk, s)
+}
+
+func (lexer *Lexer) next(n int) {
+	lexer.chunk = lexer.chunk[n:]
+}
+
+func isWhiteSpace(c byte) bool {
+	switch c {
+	case '\t', '\n', '\v', '\f', '\r', ' ':
+		return true
+	}
+	return false
+}
+
+func isNewLine(c byte) bool {
+	return c == '\r' || c == '\n'
+}
+
+func (lexer *Lexer) skipComment() {
+	lexer.next(2)        // skip '--'
+	if lexer.test("[") { // is long comment ?
+		if reOpeningLongBracket.FindString(lexer.chunk) != "" {
+			lexer.scanLongString()
+			return
+		}
+	}
+
+	// short comment
+	for len(lexer.chunk) > 0 && !isNewLine(lexer.chunk[0]) {
+		lexer.next(1)
+	}
+}
+
+func (lexer *Lexer) scanLongString() string {
+	openingLongBracket := reOpeningLongBracket.FindString(lexer.chunk)
+	if openingLongBracket == "" {
+		lexer.error("invalid long string delimiter near %s", lexer.chunk[0:2])
+	}
+
+	closingLongBracket := strings.Replace(openingLongBracket, "[", "]", -1)
+	closingLongBracketIdx := strings.Index(lexer.chunk, closingLongBracket)
+	if closingLongBracketIdx < 0 {
+		lexer.error("unfinished long static or comment")
+	}
+
+	str := lexer.chunk[len(openingLongBracket):closingLongBracketIdx]
+	lexer.line += strings.Count(str, "\n")
+	if len(str) > 0 && str[0] == '\n' {
+		str = str[1:]
+	}
+	return str
+}
+
+func (lexer *Lexer) error(f string, a ...interface{}) {
+	err := fmt.Sprintf(f, a...)
+	err = fmt.Sprintf("%s:%d: %s", lexer.chunkName, lexer.line, err)
+	panic(err)
+}
+
+func (lexer *Lexer) scanShortString() string {
+	if str := reShortStr.FindString(lexer.chunk); str != "" {
+		lexer.next(len(str))
+		// 去掉字符两端的引号
+		str = str[1 : len(str)-1]
+		// 是否有换行符
+		if strings.Index(str, `\`) >= 0 {
+			lexer.line += len(reNewLine.FindAllString(str, -1))
+			str = lexer.escape(str)
+		}
+		return str
+	}
+
+	lexer.error("unfinished string")
+	return ""
+}
+
+// 处理转义字符
+func (lexer *Lexer) escape(str string) string {
+	var buf bytes.Buffer
+
+	for len(str) > 0 {
+		if str[0] != '\\' {
+			buf.WriteByte(str[0])
+			str = str[1:]
+			continue
+		}
+		if len(str) == 1 {
+			lexer.error("unfinished string")
+		}
+		switch str[1] {
+		case 'a':
+			buf.WriteByte('\a')
+			str = str[2:]
+			continue
+		case 'b':
+			buf.WriteByte('\b')
+			str = str[2:]
+			continue
+		case 'f':
+			buf.WriteByte('\f')
+			str = str[2:]
+			continue
+		case 'n':
+			buf.WriteByte('\n')
+			str = str[2:]
+			continue
+		case '\n':
+			buf.WriteByte('\n')
+			str = str[2:]
+			continue
+		case 'r':
+			buf.WriteByte('\r')
+			str = str[2:]
+			continue
+		case 't':
+			buf.WriteByte('\t')
+			str = str[2:]
+			continue
+		case 'v':
+			buf.WriteByte('\v')
+			str = str[2:]
+			continue
+		case '"':
+			buf.WriteByte('"')
+			str = str[2:]
+			continue
+		case '\'':
+			buf.WriteByte('\'')
+			str = str[2:]
+			continue
+		case '\\':
+			buf.WriteByte('\\')
+			str = str[2:]
+			continue
+		case '0', '1', '2', '3', '4', '5', '6', '7', '8', '9': // \ddd 十进制数字
+			if found := reDecEscapeSeq.FindString(str); found != "" {
+				d, _ := strconv.ParseInt(found[1:], 10, 32)
+				if d <= 0xFF {
+					buf.WriteByte(byte(d))
+					str = str[len(found):]
+					continue
+				}
+				lexer.error("decimal escape too large near '%s'", found)
+			}
+		case 'x': // xXX 16进制数字
+			if found := reHexEscapeSeq.FindString(str); found != "" {
+				d, _ := strconv.ParseInt(found[2:], 16, 32)
+				buf.WriteByte(byte(d))
+				str = str[len(found):]
+				continue
+			}
+		case 'u': // \u{XXX} unicode
+			if found := reUnicodeEscapeSeq.FindString(str); found != "" {
+				d, err := strconv.ParseInt(found[3:len(found)-1], 16, 32)
+				if err == nil && d <= 0x10FFFF {
+					//rune是Go语言中一种特殊的数据类型,它是int32的别名,几乎在所有方面等同于int32,用于区分字符值和整数值。
+					//补充：golang中的字符有两种，uint8（byte）代表ASCII的一个字符，rune代表一个utf-8字符。
+					//理解：当需要处理中文、日文或者其他复合字符时，则需要用到rune类型，rune实际是一个int32.
+					buf.WriteRune(rune(d))
+					str = str[len(found):]
+					continue
+				}
+				lexer.error("UTF-8 value too large near '%s'", found)
+			}
+		case 'z': // 字符串结尾
+			str = str[2:]
+			for len(str) > 0 && isWhiteSpace(0) {
+				str = str[1:]
+			}
+			continue
+		}
+	}
+	return buf.String()
+}
+
+func (lexer *Lexer) scanNumber() string {
+	return lexer.scan(reNumber)
+}
+
+func (lexer *Lexer) scanIdentifier() string {
+	return lexer.scan(reIdentifier)
+}
+
+func (lexer *Lexer) scan(re *regexp.Regexp) string {
+	if token := re.FindString(lexer.chunk); token != "" {
+		lexer.next(len(token))
+		return token
+	}
+	panic("unreachable")
+}
---
+++ go/ch14/src/luago/state/lua_table.go
@@ -0,0 +1,136 @@
+package state
+
+import (
+	"luago/number"
+	"math"
+)
+
+type luaTable struct {
+	metatable *luaTable // 元表
+	arr       []luaValue
+	_map      map[luaValue]luaValue
+	keys      map[luaValue]luaValue
+	changed   bool
+}
+
+func (table *luaTable) nextKey(key luaValue) luaValue {
+	if table.keys == nil || key == nil {
+		table.initKeys()
+		table.changed = false
+	}
+	return table.keys[key]
+}
+
+func (table *luaTable) initKeys() {
+	table.keys = make(map[luaValue]luaValue)
+	var key luaValue = nil
+	// 先遍历数组部分
+	for i, v := range table.arr {
+		if v != nil {
+			table.keys[key] = int64(i + 1)
+			key = int64(i + 1)
+		}
+	}
+	// 再遍历map部分
+	for k, v := range table._map {
+		if v != nil {
+			table.keys[key] = k
+			key = k
+		}
+	}
+}
+
+func newLuaTable(nArr, nRec int) *luaTable {
+	t := &luaTable{}
+	if nArr > 0 { // 表可能是当作数组使用的 先创建数组
+		t.arr = make([]luaValue, nArr)
+	}
+	if nRec > 0 { // 创建map
+		t._map = make(map[luaValue]luaValue, nRec)
+	}
+	return t
+}
+
+func (table *luaTable) get(key luaValue) luaValue {
+	key = _floatToInteger(key)
+	if idx, ok := key.(int64); ok {
+		if idx >= 1 && idx <= int64(len(table.arr)) {
+			return table.arr[idx-1]
+		}
+	}
+	return table._map[key]
+}
+
+func _floatToInteger(key luaValue) luaValue {
+	if f, ok := key.(float64); ok {
+		if i, ok := number.FloatToInteger(f); ok {
+			return i
+		}
+	}
+	return key
+}
+
+func (table *luaTable) put(key, val luaValue) {
+	if key == nil {
+		panic("table index is nil")
+	}
+	if f, ok := key.(float64); ok && math.IsNaN(f) {
+		panic("table index is NaN")
+	}
+	key = _floatToInteger(key)
+
+	if idx, ok := key.(int64); ok && idx >= 1 {
+		arrLen := int64(len(table.arr))
+		if idx <= arrLen {
+			table.arr[idx-1] = val
+			if idx == arrLen && val == nil {
+				table._shrinkArray()
+			}
+			return
+		}
+		if idx == arrLen+1 {
+			delete(table._map, key) // key可能已经存在了map中
+			if val != nil {
+				table.arr = append(table.arr, val)
+				table._expandArray()
+			}
+			return
+		}
+	}
+
+	if val != nil {
+		if table._map == nil {
+			table._map = make(map[luaValue]luaValue, 8) // 默认容量为8
+		}
+		table._map[key] = val
+	} else {
+		delete(table._map, key)
+	}
+}
+
+func (table *luaTable) _shrinkArray() {
+	for i := len(table.arr) - 1; i >= 0; i-- {
+		if table.arr[i] == nil {
+			table.arr = table.arr[0:i]
+		}
+	}
+}
+
+func (table *luaTable) _expandArray() {
+	for idx := int64(len(table.arr)) + 1; true; idx++ {
+		if val, found := table._map[idx]; found {
+			delete(table._map, idx)
+			table.arr[idx] = val
+		} else {
+			break
+		}
+	}
+}
+
+func (table *luaTable) len() int {
+	return len(table.arr)
+}
+
+func (table *luaTable) hasMetaField(fieldName string) bool {
+	return table.metatable != nil && table.metatable.get(fieldName) != nil
+}
